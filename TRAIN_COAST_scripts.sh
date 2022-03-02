93b962a62bb7c550bc665ae14f1e8e2ad291e175
Jian Zhang <jianzhangcs@aliyun.com>
2022-03-02 14:22:06 +0800

    Add files via upload

---
+++ TRAIN_COAST_scripts.sh
@@ -0,0 +1,5 @@
+python TRAIN_COAST.py  --cs_ratio 10 --layer_num 20
+python TRAIN_COAST.py  --cs_ratio 20 --layer_num 20
+python TRAIN_COAST.py  --cs_ratio 30 --layer_num 20
+python TRAIN_COAST.py  --cs_ratio 40 --layer_num 20
+python TRAIN_COAST.py  --cs_ratio 50 --layer_num 20
